035c452e543fc638feb68f8a753c2f16e1923547
Oriol Gual <oriol.gual@gmail.com>
2010-06-23 16:33:33 +0800

    Added if_first, unless_fist, if_last, unless_last like children:each

--- lib/random_child.rb
+++ lib/random_child.rb
@@ -38,4 +38,79 @@ module RandomChild
       result
     end
   end
+
+  desc %{
+    Renders the tag contents only if the current page is the first child in the context of
+    a children:random tag
+    
+    *Usage:*
+    
+    <pre><code><r:children:random>
+      <r:if_first >
+        ...
+      </r:if_first>
+    </r:children:random>
+    </code></pre>
+    
+  }
+  tag 'children:random:if_first' do |tag|
+    tag.expand if tag.locals.first_child
+  end
+
+  
+  desc %{
+    Renders the tag contents unless the current page is the first child in the context of
+    a children:random tag
+    
+    *Usage:*
+    
+    <pre><code><r:children:random>
+      <r:unless_first >
+        ...
+      </r:unless_first>
+    </r:children:random>
+    </code></pre>
+    
+  }
+  tag 'children:random:unless_first' do |tag|
+    tag.expand unless tag.locals.first_child
+  end
+  
+  desc %{
+    Renders the tag contents only if the current page is the last child in the context of
+    a children:random tag
+    
+    *Usage:*
+    
+    <pre><code><r:children:random>
+      <r:if_last >
+        ...
+      </r:if_last>
+    </r:children:random>
+    </code></pre>
+    
+  }
+  tag 'children:random:if_last' do |tag|
+    tag.expand if tag.locals.last_child
+  end
+
+  
+  desc %{
+    Renders the tag contents unless the current page is the last child in the context of
+    a children:random tag
+    
+    *Usage:*
+    
+    <pre><code><r:children:random>
+      <r:unless_last >
+        ...
+      </r:unless_last>
+    </r:children:random>
+    </code></pre>
+    
+  }
+  tag 'children:random:unless_last' do |tag|
+    tag.expand unless tag.locals.last_child
+  end
+  
 end
